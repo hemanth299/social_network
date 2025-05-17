76d31d9737350b7518c04c12c2650fdc813e4ba8
Hemanth <hemanthyerra9@gmail.com>
2025-05-17 18:01:10 +0530

    learned about this zustand an amazing state management tool, used it in some of the components, backend final touch coming up

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,44 @@
-import List from "./components/list/list";
+import { useEffect } from "react";
 import Chat from "./components/chat/Chat";
 import Detail from "./components/detail/Detail";
+import List from "./components/list/List";
 import Login from "./components/login/Login";
 import Notification from "./components/notification/Notification";
-import { useEffect } from "react";
-import { auth } from "./lib/firebase"; // Adjust the path to your Firebase configuration file
 import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./lib/firebase";
+import { useUserStore } from "./lib/userStore";
+import { useChatStore } from "./lib/chatStore";
 
-function App() {
-  const user = false;
+const App = () => {
+  const { currentUser, isLoading, fetchUserInfo } = useUserStore();
+  const { chatId } = useChatStore();
+
   useEffect(() => {
     const unSub = onAuthStateChanged(auth, (user) => {
-      console.log(user);
-    }
-    );
+      fetchUserInfo(user?.uid);
+    });
+
     return () => {
       unSub();
     };
-  }
-  , []);
+  }, [fetchUserInfo]);
+
+  if (isLoading) return <div className="loading">Loading...</div>;
+
   return (
     <div className="container">
-      {user ? (
+      {currentUser ? (
         <>
           <List />
-          <Chat />
-          <Detail />
+          {chatId && <Chat />}
+          {chatId && <Detail />}
         </>
       ) : (
         <Login />
       )}
-      <Notification/>
+      <Notification />
     </div>
   );
-}
+};
 
 export default App;
